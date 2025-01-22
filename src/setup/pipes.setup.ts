7803499b83cd0959dfd2bd712c3738024360754c
news1d <news1dee@yandex.ru>
2025-01-23 02:24:33 +0300

    Added errorFormatter and pipesSetup modified

--- src/setup/pipes.setup.ts
+++ src/setup/pipes.setup.ts
@@ -1,12 +1,48 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
+import { ValidationError } from '@nestjs/common';
+import { BadRequestDomainException } from '../core/exceptions/domain-exceptions';
+import { ObjectIdValidationTransformationPipe } from '../core/pipes/object-id-validation-transformation-pipe.service';
+
+type ErrorResponse = { message: string; field: string };
+
+export const errorFormatter = (
+  errors: ValidationError[],
+  errorMessage?: any,
+): ErrorResponse[] => {
+  const errorsForResponse = errorMessage || [];
+
+  for (const error of errors) {
+    if (!error?.constraints && error?.children?.length) {
+      errorFormatter(error.children, errorsForResponse);
+    } else if (error?.constraints) {
+      const constrainKeys = Object.keys(error.constraints);
+
+      for (const key of constrainKeys) {
+        errorsForResponse.push({
+          message: error.constraints[key]
+            ? `${error.constraints[key]}; Received value: ${error?.value}`
+            : '',
+          field: error.property,
+        });
+      }
+    }
+  }
+
+  return errorsForResponse;
+};
+
 export function pipesSetup(app: INestApplication) {
   app.useGlobalPipes(
+    new ObjectIdValidationTransformationPipe(),
     new ValidationPipe({
-      //class-transformer создает экземпляр dto
-      //соответственно применятся значения по-умолчанию
-      //и методы классов dto
       transform: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errorFormatter(errors);
+
+        throw new BadRequestDomainException(formattedErrors);
+      },
     }),
   );
 }
