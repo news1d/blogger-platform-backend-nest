4a845fa8a0aa7cc07206c75bd763185059a24a3d
news1d <news1dee@yandex.ru>
2025-01-23 02:31:44 +0300

    Comments deleted

--- src/setup/global-prefix.setup.ts
+++ src/setup/global-prefix.setup.ts
@@ -3,6 +3,5 @@ import { INestApplication } from '@nestjs/common';
 export const GLOBAL_PREFIX = 'api';
 
 export function globalPrefixSetup(app: INestApplication) {
-  //специальный метод, который добавляет ко всем маршрутам /GLOBAL_PREFIX
   app.setGlobalPrefix(GLOBAL_PREFIX);
 }
