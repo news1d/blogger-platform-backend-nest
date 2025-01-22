f3f2134274bc173878a4775d173f5d0a983ed478
news1d <news1dee@yandex.ru>
2025-01-23 02:43:17 +0300

    Bearer authorization implemented

---
+++ src/features/user-accounts/guards/bearer/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserContextDto } from '../dto/user-context.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: UserContextDto): Promise<UserContextDto> {
+    return payload;
+  }
+}
