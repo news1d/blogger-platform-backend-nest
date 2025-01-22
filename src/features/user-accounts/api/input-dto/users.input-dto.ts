6bd7f4301c8977b141f65eae7289eb4353d85480
news1d <news1dee@yandex.ru>
2025-01-23 02:33:06 +0300

    Added field validation

--- src/features/user-accounts/api/input-dto/users.input-dto.ts
+++ src/features/user-accounts/api/input-dto/users.input-dto.ts
@@ -1,5 +1,34 @@
+import { IsEmail, IsString, Length, Matches } from 'class-validator';
+import { Trim } from '../../../../core/decorators/transform/trim';
+import {
+  loginConstraints,
+  passwordConstraints,
+} from '../../domain/user.entity';
+
 export class CreateUserInputDto {
+  @Trim()
+  @IsString()
+  @Length(loginConstraints.minLength, loginConstraints.maxLength)
+  @Matches(loginConstraints.match, {
+    message:
+      'Login must contain only letters, numbers, underscores, or hyphens.',
+  })
   login: string;
+
+  @Trim()
+  @IsString()
+  @Length(passwordConstraints.minLength, passwordConstraints.maxLength)
   password: string;
+
+  @Trim()
+  @IsString()
+  @IsEmail()
+  email: string;
+}
+
+export class EmailInputDto {
+  @Trim()
+  @IsString()
+  @IsEmail()
   email: string;
 }
