fabf0373ea255e8e6fe93512bd1eb314f6aceb2f
news1d <news1dee@yandex.ru>
2025-02-01 02:02:10 +0300

    Added validation

--- src/features/bloggers-platform/comments/api/input-dto/comments.input-dto.ts
+++ src/features/bloggers-platform/comments/api/input-dto/comments.input-dto.ts
@@ -1,3 +1,8 @@
-export class CreateCommentInputModel {
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateCommentInputDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(20, 300)
   content: string;
 }
