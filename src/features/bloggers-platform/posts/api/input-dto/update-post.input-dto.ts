9285b6f7252b0641e0010d09097cb774a56be176
news1d <news1dee@yandex.ru>
2025-02-03 02:32:45 +0300

    Added validator to check blogId in update posts input dto

--- src/features/bloggers-platform/posts/api/input-dto/update-post.input-dto.ts
+++ src/features/bloggers-platform/posts/api/input-dto/update-post.input-dto.ts
@@ -1,6 +1,7 @@
 import { UpdatePostDto } from '../../dto/create-post.dto';
 import { Trim } from '../../../../../core/decorators/transform/trim';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { BlogIdExists } from '../../application/validators/blogIdExists';
 
 export class UpdatePostInputDto implements UpdatePostDto {
   @Trim()
@@ -24,5 +25,6 @@ export class UpdatePostInputDto implements UpdatePostDto {
   @Trim()
   @IsNotEmpty()
   @IsString()
+  @BlogIdExists()
   blogId: string;
 }
