08c6a0d604985e9078493c8601fcd26db2e8c830
news1d <news1dee@yandex.ru>
2025-02-04 23:21:20 +0300

    UseContainer for validation moved to separate file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
 import { CoreConfig } from './core/core.config';
-import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +9,6 @@ async function bootstrap() {
   const appConfig = app.get<CoreConfig>(CoreConfig);
 
   app.enableCors();
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   appSetup(app);
   await app.listen(appConfig.port);
 }
--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -3,10 +3,12 @@ import { INestApplication } from '@nestjs/common';
 import { globalPrefixSetup } from './global-prefix.setup';
 import { swaggerSetup } from './swagger.setup';
 import { exceptionFilterSetup } from './exception-filter.setup';
+import { validationConstraintSetup } from './validation-constraint.setup';
 
 export function appSetup(app: INestApplication) {
   pipesSetup(app);
   globalPrefixSetup(app);
   swaggerSetup(app);
+  validationConstraintSetup(app);
   exceptionFilterSetup(app);
 }
