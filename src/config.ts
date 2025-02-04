0cce19878f730ff24ae360d0445a99b27dab9036
news1d <news1dee@yandex.ru>
2025-02-05 00:02:35 +0300

    Some refactoring

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
-    process.env.ENV_FILE_PATH?.trim(),
+    process.env.ENV_FILE_PATH?.trim() || '',
     `.env.${process.env.NODE_ENV}.local`,
     `.env.${process.env.NODE_ENV}`,
     '.env.development',
