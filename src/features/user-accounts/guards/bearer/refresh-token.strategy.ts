b2101848a13f8d90fd5c7e5f8ec3f15114ca040c
news1d <news1dee@yandex.ru>
2025-02-16 17:58:24 +0300

    Added authorization with refresh token

---
+++ src/features/user-accounts/guards/bearer/refresh-token.strategy.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { JwtConfig } from '../../config/jwt.config';
+import { UserContextDto } from '../dto/user-context.dto';
+import { BlacklistQueryRepository } from '../../infrastructure/query/blacklist.query-repository';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(
+    private jwtConfig: JwtConfig,
+    private blacklistQueryRepository: BlacklistQueryRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.refreshToken, // Берём токен из куков
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: jwtConfig.refreshSecret,
+    });
+  }
+
+  async validate(
+    payload: UserContextDto,
+    req: Request,
+  ): Promise<UserContextDto> {
+    const refreshToken = req.cookies?.refreshToken;
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token is missing');
+    }
+
+    const isBlacklisted =
+      await this.blacklistQueryRepository.getToken(refreshToken);
+
+    if (isBlacklisted) {
+      throw new UnauthorizedException('Refresh token is invalid');
+    }
+
+    return payload;
+  }
+}
