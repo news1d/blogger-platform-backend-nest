21ebe57f166ff3cf54c523b84298d5832b9ffd8c
news1d <news1dee@yandex.ru>
2025-02-16 21:47:56 +0300

    Modified: Data is extracted from cookie

--- src/features/user-accounts/guards/decorators/param/extract-device-from-request.decorator.ts
+++ src/features/user-accounts/guards/decorators/param/extract-device-from-request.decorator.ts
@@ -2,16 +2,15 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import jwt from 'jsonwebtoken';
 import { DeviceContextDto } from '../../dto/device-context.dto';
 
-export const ExtractDeviceFromRequest = createParamDecorator(
+export const ExtractDeviceFromCookie = createParamDecorator(
   (data: unknown, context: ExecutionContext): DeviceContextDto => {
     const request = context.switchToHttp().getRequest();
 
-    const authHeader = request.get('Authorization');
-    if (!authHeader) {
-      throw new Error('there is no authHeader in the request object!');
-    }
+    const refreshToken = request.cookies?.refreshToken;
 
-    const refreshToken = authHeader.replace('Bearer', '').trim();
+    if (!refreshToken) {
+      throw new Error('Refresh token is missing in cookies!');
+    }
 
     try {
       const decoded: any = jwt.decode(refreshToken);
--- src/features/user-accounts/guards/decorators/param/extract-refresh-token-from-request.decorator.ts
+++ src/features/user-accounts/guards/decorators/param/extract-refresh-token-from-request.decorator.ts
@@ -1,17 +1,16 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { RefreshTokenContextDto } from '../../dto/refreshToken-context.dto';
 
-export const ExtractRefreshTokenFromRequest = createParamDecorator(
+export const ExtractRefreshTokenFromCookie = createParamDecorator(
   (data: unknown, context: ExecutionContext): RefreshTokenContextDto => {
     const request = context.switchToHttp().getRequest();
 
-    const authHeader = request.get('Authorization');
-    if (!authHeader) {
-      throw new Error('there is no authHeader in the request object!');
+    const refreshToken = request.cookies?.refreshToken;
+
+    if (!refreshToken) {
+      throw new Error('Refresh token is missing in cookies!');
     }
 
-    const refreshToken = authHeader.replace('Bearer', '').trim();
-
-    return refreshToken;
+    return { token: refreshToken };
   },
 );
