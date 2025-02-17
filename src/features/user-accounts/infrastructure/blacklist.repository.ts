1886853a94c26d0938eba7ab8d231a2f36a461e3
news1d <news1dee@yandex.ru>
2025-02-17 16:44:09 +0300

    lastActivate renamed to lastActiveDate

--- src/features/user-accounts/infrastructure/blacklist.repository.ts
+++ src/features/user-accounts/infrastructure/blacklist.repository.ts
@@ -1,8 +1,21 @@
-import { BlacklistDocument } from '../domain/blacklist.entity';
+import {
+  Blacklist,
+  BlacklistDocument,
+  BlacklistModelType,
+} from '../domain/blacklist.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class BlacklistRepository {
+  constructor(
+    @InjectModel(Blacklist.name) private BlacklistModel: BlacklistModelType,
+  ) {}
+  async getToken(token: string): Promise<boolean> {
+    const result = await this.BlacklistModel.findOne({ token: token });
+    return !!result;
+  }
+
   async save(token: BlacklistDocument): Promise<void> {
     await token.save();
   }
